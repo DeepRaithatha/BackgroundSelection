452386c21f4cf08dee7af86f6ad3b75228e51ceb
DeepRaithatha <raithathadeep@gmail.com>
2014-11-28 13:58:46 -0500

    Initial commit

---
+++ core/src/com/mygdx/game/MainActivity.java
@@ -0,0 +1,85 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
+public class MainActivity extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture img;
+    Texture img2;
+    Texture img3;
+    Texture[] tile = new Texture[3];
+    Stage stage;
+    TextButton button;
+    TextButton.TextButtonStyle textButtonStyle;
+    BitmapFont font;
+    Skin skin;
+    TextureAtlas buttonAtlas;
+    int nColour = 1;
+    Sound sound;
+    Texture TempImg;
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        stage = new Stage();
+//        tile[0] = new Texture(Gdx.files.internal("cave.jpg"));
+//        tile[1] = new Texture(Gdx.files.internal("Rainforest.jpg"));
+//        tile[2] = new Texture(Gdx.files.internal("city.jpg"));
+
+        Gdx.input.setInputProcessor(stage);
+        font = new BitmapFont();
+        skin = new Skin();
+        buttonAtlas = new TextureAtlas("button.pack"); //
+        skin.addRegions(buttonAtlas);
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("UP");
+        textButtonStyle.down = skin.getDrawable("DOWN");
+        textButtonStyle.checked = skin.getDrawable("DOWN");
+        button = new TextButton("", textButtonStyle);
+        button.setSize(Gdx.graphics.getWidth()/6,Gdx.graphics.getWidth()/6);
+        stage.addActor(button);
+        button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+//                TempImg = tile[0];
+//                for(int i = 0;i<tile.length-1;i++){
+//                    tile[i]=tile[i+1];
+//
+//                }
+//                tile[tile.length -1] = TempImg;
+            }
+
+
+                /*img3 = img;
+                img = img2;
+                img2 = img3;*/
+
+
+        });
+    }
+
+
+
+
+    @Override
+    public void render () {
+        batch.begin();
+        batch.draw(tile[0], 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        batch.end();
+        super.render();
+        stage.draw();
+    }
+}
+
